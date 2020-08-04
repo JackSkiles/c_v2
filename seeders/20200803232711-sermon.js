2c23d6bd6c5261578d1706e8224b494d8c67bc2f
Jack Skiles <jackskiles420@gmail.com>
2020-08-03 20:13:58 -0400

    maybe fixes

--- seeders/20200803232711-sermon.js
+++ seeders/20200803232711-sermon.js
@@ -14,6 +14,7 @@ module.exports = {
   },
 
   down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('Sermon', null, {});
     /**
      * Add commands to revert seed here.
      *
