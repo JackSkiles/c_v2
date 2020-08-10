f17e2e006f49c7aa578306dbce78d074ee1a6506
Jack Skiles <jackskiles420@gmail.com>
2020-08-10 14:27:51 -0400

    updated contact form

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -1,62 +1,39 @@
-import React, { Component } from 'react'
-import axios from 'axios';
+import React from 'react';
+import emailjs from 'emailjs-com';
+import { Form, Button } from 'react-bootstrap';
+import './pages.css'
 
-class Contact extends Component {
+export default function Contact() {
 
-    state = {
-        name: '',
-        message: '',
-        email: '',
-        sent: false,
-        buttonText: 'Send Message'
-    }
-    formSubmit = (e) => {
-      e.preventDefault()
-    
-      this.setState({
-          buttonText: '...sending'
-      })
-    
-      let data = {
-          name: this.state.name,
-          email: this.state.email,
-          message: this.state.message
-      }
-      
-      axios.post('API_URI', data)
-      .then( res => {
-          this.setState({ sent: true }, this.resetForm())
-      })
-      .catch( () => {
-        console.log('Message not sent')
-      })
-    }
-    
-resetForm = () => {
-  this.setState({
-      name: '',
-      message: '',
-      email: '',
-      buttonText: 'Message Sent'
-  })
+  function sendEmail(e) {
+    e.preventDefault();
+
+    emailjs.sendForm('gmail', 'church', e.target, 'user_ShoPmwZqDZMyJyAme7PzU')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+      e.target.reset();
+  }
+
+  return (
+    <div className="main">
+      <div className="imgCont">
+                <img className="topImg" src="sports.jpg"></img>
+      </div>
+      <div className="mainCont">
+        <Form className="contact-form" onSubmit={sendEmail}>
+          <Form.Control type="hidden" name="contact_number" />
+          <Form.Label>Name</Form.Label>
+          <Form.Control type="text" name="user_name" />
+          <Form.Label>Email</Form.Label>
+          <Form.Control type="email" name="user_email" />
+          <Form.Label>Message</Form.Label>
+          <Form.Control name="message" />
+          <Button type="submit" value="Send">Send</Button>
+        </Form>
+      </div>
+    </div>
+  );
 }
-    render() {
-        return(
-          <form className="contact-form" onSubmit={ (e) => this.formSubmit(e)}>
-            <label class="message" htmlFor="message-input">Your Message</label>
-            <textarea onChange={e => this.setState({ message: e.target.value})} name="message" class="message-input" type="text" placeholder="Please write your message here" value={this.state.message} required/>
-
-            <label class="message-name" htmlFor="message-name">Your Name</label>
-            <input onChange={e => this.setState({ name: e.target.value})} name="name" class="message-name" type="text" placeholder="Your Name" value={this.state.name}/>
-
-            <label class="message-email" htmlFor="message-email">Your Email</label>
-            <input onChange={(e) => this.setState({ email: e.target.value})} name="email" class="message-email" type="email" placeholder="your@email.com" required value={this.state.email} />
-
-            <div className="button--container">
-                <button type="submit" className="button button-primary">{ this.state.buttonText }</button>
-            </div>
-          </form>
-        );
-    }
-}
-export default Contact;
