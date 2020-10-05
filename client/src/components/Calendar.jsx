b3dd3b1f0f4ef202ac1d25bbfc45598ad789aff0
Jack Skiles <jackskiles420@gmail.com>
2020-10-04 20:27:52 -0400

    some additions to the calendar

--- client/src/components/Calendar.jsx
+++ client/src/components/Calendar.jsx
@@ -6,6 +6,11 @@ import paginationFactory from 'react-bootstrap-table2-paginator';
 import BootstrapTable from 'react-bootstrap-table-next';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
+import BigCalendar from 'react-big-calendar';
+import moment from 'moment';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+// import './BookingCalendar.css';
+// import NewBookingButton from './NewBookingButton'
 
 
 
