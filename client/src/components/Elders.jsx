837eae02379f3d1e8c3f06b07085e05e5cd5cac8
Jack Skiles <jackskiles420@gmail.com>
2020-10-20 19:10:41 -0400

    updated elders page

--- client/src/components/Elders.jsx
+++ client/src/components/Elders.jsx
@@ -7,16 +7,26 @@ export default function Elders() {
             <div className="imgCont">
                 <img className="topImg" src="bible2.jpg"></img>
             </div>
-            <div className="mainCont">
+            <div className="mainCont3">
                 <div className="textCont">
                     <h2>Current Pastors:</h2>
-                    <h3>Andrew Huffman</h3>
-                    <h3>James Hindman</h3>
+                    <div className="picCont">
+                        <div className="pic">
+                            <h3>Andrew Huffman</h3>
+                            <img src='IMG_3219.jpg' id='pic'/>
+                        </div>
+                        <div className="pic">
+                            <h3>James Hindman</h3>
+                            <img src='IMG_3219.jpg' id='pic'/>
+                        </div>
+                    </div>
                 </div>
-                <div className="textCont">
+            </div>
+            <div className="mainCont3">
+            <div className="textCont">
                     <h2>Previous Elders:</h2>
                     <h3>Joe Hildreth</h3>
-                </div>
+                </div> 
             </div>
             <Footer />
         </div>
