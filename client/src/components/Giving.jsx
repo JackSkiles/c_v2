3bc1c108111c3ba4ea2f3c5d3e8e3738753e19a4
Jack Skiles <jackskiles420@gmail.com>
2020-10-22 16:27:26 -0400

    added stripe checkout features

--- client/src/components/Giving.jsx
+++ client/src/components/Giving.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+import StripeCheckout from 'react-stripe-checkout';
 
 export default function Giving() {
+
+    function handleToken(token, addresses) {
+        console.log({ token, address })
+    }
     return (
         <div>
-            
+            <StripeCheckout 
+            stripeKey="pk_test_51HfAIJGuKajdMWRykHExDj6ZSCNHsex0qYE0IfOm2jz2ZOkdb02sNhiNpgj7ygBGWvFle6HdU3tahaE5lDHF5Itk00qgcIl0lU"
+            token={} />
         </div>
     )
 }
