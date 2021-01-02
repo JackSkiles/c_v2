f0a668a9801624400bc6d7e3624cff4dacf0bc2d
Jack Skiles <jackskiles420@gmail.com>
2021-01-02 17:32:12 -0500

    added info to distinctions page

--- client/src/components/Beliefs.jsx
+++ client/src/components/Beliefs.jsx
@@ -9,15 +9,26 @@ export default function Beliefs() {
                 <img className="topImg" src="bible.jpg"></img>
             </div>
             <div className="mainCont">
-                <div className="textCont">
                     <h2>Our Distinctions:</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur maximus auctor tellus, quis commodo purus congue ac.
-                            Integer blandit pellentesque commodo. Aliquam est tortor, venenatis vel euismod a, pharetra nec elit. Nulla vitae magna luctus orci suscipit mattis non at turpis. Sed non tristique felis. Nam vitae sem sed mauris tincidunt pretium. Nunc consectetur augue eu ipsum blandit, a sodales sem aliquam. Nulla orci nulla, blandit et pharetra et, cursus vel purus. Vestibulum varius et risus sit amet mollis. Nunc porttitor egestas turpis, ut fermentum nisi sodales sed. Nulla vehicula finibus nulla, at sollicitudin sem condimentum non. Vivamus vel nisl quam. Cras nunc nulla, aliquet non tellus sed, lobortis pellentesque eros. Ut faucibus bibendum dapibus.</p>
+                    <h5>Articles of Faith</h5>
+                <div className="textCont2">
+                    <p>
+                        We believe that there is but one true and living God and that in the Godhead are three persons, the Father, The Word, or Son, and the Holy Ghost which three are one.
+                        We believe that the Old and New Testament Scriptures were given by inspiration of God and we accept them as being the only complete and unerring rule of faith and practice.
+                        We believe that God has always pursued His Own infinitely wise plan in all of His works and ways and that He will ever continue so to do; hence, all things brought to pass by Him are but the result of His holy, wise and determinate counsel from eternity.
+                        We believe that Adam, though created in the image of his Master, Who pronounced him very good, did of his own volition, willfully transgress the law of God; and, as a consequence, became a fallen and totally depraved creature and all mankind with him.
+                        We believe that Redemption, Regeneration, Sanctification, Justification, and Salvation are by virtue of the birth, life, death, resurrection, and mediation of Jesus Christ, and in no other way; and that all the graces of the spirit are referable to the church of God.
+                        We believe that all the saints were predestinated unto the adoption of children by Jesus Christ, elected to eternal salvation according to the foreknowledge of God the Father; that they will all be called with a holy calling and the righteousness of Christ actually imputed to them by the effectual working of the Holy Spirit, so that none of the heirs of promise can be eternally lost.
+                        We believe that all the elect of God were chosen in Christ Jesus before the foundation of the world, that they should be holy and without blame before Him in love.
+                        We believe that immersion, upon profession of faith in Christ, in the name of the sacred Trinity, by one duly authorized by the church to administer the ordinances of God in the church, is gospel baptism, and that only; and that all persons so baptized who shall continue to walk circumspectly shall have the right to partake of the Lord’s Supper, and no other.
+                        We believe that the Church of Christ is a body corporate, possessing full power to govern herself; and having the exclusive right to receive and to discipline her own members, knowing no rule but the Word of God, she is, therefore, independent.
+                        We believe that there will be a resurrection of the dead, both of the just and the unjust, and that the happiness of the righteous, and the punishment of the wicked will both be eternal. We believe that the grace of God that bringeth salvation teaches us that, denying ungodliness and worldly lust, we should live soberly, righteously, and godly in this present world; and that it is, therefore, the duty of all church members to be jointly and severally engaged in every good work for the glory of God and the honor of the Christian religion.
+                    </p>
                 </div>
                 <div className="textCont">
                     <h2>“</h2>
                     <p>
-                    For by grace are ye saved through faith; and that not of yourselves: it is the gift of God: Not of works, lest any man should boast.</p>
+                        For by grace are ye saved through faith; and that not of yourselves: it is the gift of God: Not of works, lest any man should boast.</p>
                     <p>— Ephesians 2:8-9</p>
                 </div>
             </div>
