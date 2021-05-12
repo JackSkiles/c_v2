6f08af6889c3eea4e1c4feb4ba522193897d68f8
Jack Skiles <jackskiles420@gmail.com>
2021-05-12 19:04:58 -0400

    some updates for hosting

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
 import 'react-bootstrap-table2-paginator/dist/react-bootstrap-table2-paginator.min.css';
 
+const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
+const rootElement = document.getElementById('root');
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename={baseUrl}>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  rootElement
 );
 
 // If you want your app to work offline and load faster, you can change
